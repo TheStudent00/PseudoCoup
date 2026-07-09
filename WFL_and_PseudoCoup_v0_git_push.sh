067194a26ca4260a6c910aa97b22279bdfdc26a7
TheStudent00 <TheStudent00@users.noreply.github.com>
2026-07-09 15:39:10 -0400

    update

--- WFL_and_PseudoCoup_v0_git_push.sh
+++ WFL_and_PseudoCoup_v0_git_push.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
-cd ~/Programming/PseudoCoup_v0
-git add .
-git commit -m "update"
-git push
+
 cd ~/Programming/WFL_MixingCenter
 git add .
 git commit -m "update"
 git push
+
+cd ~/Programming/PseudoCoup_v0
+git add .
+git commit -m "update"
+git push
+
